8eb18a9d3ff3da8fd85a4cf40d630fc6aae196ec
AgustinCarbajo <105457478+AgustinCarbajo@users.noreply.github.com>
2022-05-25 19:21:59 -0300

    Corrección arregle la palabra mayor en line 21

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ btnEnviar.addEventListener("click", () => {
   if (numero1 > numero2 && numero1 > numero3) {
     console.log("El mayor es " + numero1);
   } else if (numero2 > numero1 && numero2 > numero3) {
-    console.log("El mayour es " + numero2);
+    console.log("El mayor es " + numero2);
   } else {
     console.log("El mayor es " + numero3);
   }
